0321675d0bc9f076ef4ec2c5738049464dc18803
Judely <judelymarenco08@gmail.com>
2025-06-22 15:11:08 -0600

    Modificacion Pro.java

--- Pro.java
+++ Pro.java
@@ -1,9 +1,11 @@
 public class Pro{
     public static void main (Strin[] args){
  int a= 50;.
- intb=50;
+ intb=100;
  String nom= "Judely";
  System.out.println("Nombre: " + nom );
+ System.out.println("Notas: "+ a+b "es");
+
     }
    
 }
