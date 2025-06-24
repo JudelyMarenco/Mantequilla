c383401b870c7df80cc554a58d82aef1229d4b31
Judely <judelymarenco@gmail.com>
2025-06-24 11:10:18 -0600

    Cambio numero

--- Pro.java
+++ Pro.java
@@ -1,9 +1,13 @@
 public class Pro{
     public static void main (Strin[] args){
  int a= 50;.
- intb=50;
+ int b =50;
+ int num= 2025;
  String nom= "Judely";
  System.out.println("Nombre: " + nom );
+ System.out.println("Suma: "+ a+b);
+ Systen.out.printlm("num carnet "+ num);
+
     }
    
 }
