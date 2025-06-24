d9887ef4d44764653478aa7270e0c3f13fcc79af
Judely <judelymarenco@gmail.com>
2025-06-24 11:53:32 -0600

    Cambio saludo

--- Pro.java
+++ Pro.java
@@ -1,10 +1,13 @@
 public class Pro{
     public static void main (Strin[] args){
  int a= 50;.
- intb=100;
- String nom= "Judely";
+ int=90;
+ int car=1491;
+ String nom= "Judely del carmen Marenco Guerrero ";
  System.out.println("Nombre: " + nom );
  System.out.println("Notas: "+ a+b "es");
+ System.out.println("hola como estas" + nom );
+ System.out.printl("Carnet :" + car)
 
     }
    
